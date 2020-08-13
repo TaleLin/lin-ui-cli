502d3c030768418fa54ebd52ed9f7bb8e3cfb26a
木荣 <372638156@qq.com>
2020-08-13 09:06:00 +0800

    refactor: 优化命令行错误提示

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,50 @@
 import program from 'commander'
 import build from './build'
 import create from './create'
-import { CLI_VERSION } from './config'
-program.version(CLI_VERSION, '-v, --version')
-program.command('build')
+import { CLI_VERSION, CLI_NAME } from './config'
+import { error, warn } from './tip-style'
+
+/**
+ * @name 未知参数错误提示
+ * @param {string} methodName
+ * @param {Function} log
+ */
+function enhanceErrorMessages(methodName: string, log: Function) {
+    program.Command.prototype[methodName] = function (...args: any) {
+        if (methodName === 'unknownOption' && this._allowUnknownOption) {
+            return
+        }
+        this.outputHelp()
+        console.log(`  ` + error(log(...args)))
+        console.log()
+        process.exit(1)
+    }
+}
+
+program
+    .version(CLI_VERSION, '-v, --version')
+    .usage('<command> [options]')
+program
+    .command('build')
+    .description('build lin ui of load on demand')
     .action(build)
-program.command('create <name>')
+program
+    .command('create <name>')
+    .usage('<name>')
+    .description(`create a new project powered by ${CLI_NAME}`)
     .action(create)
+// output help information on unknown commands
+program
+    .arguments('<command>')
+    .action((cmd) => {
+        program.outputHelp()
+        console.log()
+        console.log(`  ` + `${error(`Unknown command ${warn(cmd)}`)}`)
+        console.log()
+    })
+
+enhanceErrorMessages('missingArgument', (argName: string) => {
+    console.log()
+    return `${error(`Missing required argument ${warn(argName)}.`)}`
+})
 program.parse(process.argv);
